9a7d35d0f3df880445af8fd1265ac52e40a27845
Dinael URDANETA <dinael@gmail.com>
2015-08-18 18:38:20 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
 // $$ PLUGINS Y FUNCIONES
 $(document).ready(function() {
-
+//Smooth scroll
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: (target.offset().top - 150)
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+//Ir arriba
+    $(function() {
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 100) {
+                $("#go-top").fadeIn();
+            } else {
+                $("#go-top").fadeOut();
+            }
+        });
+//history back	
+        $('a.volver').click(function() {
+            parent.history.back();
+            return false;
+        });
 });
